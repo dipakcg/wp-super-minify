eda5284c77b2fb083fced226d6795df68b869264
Dipak C. Gajjar <me@dipakgajjar.com>
2019-07-03 17:30:32 +0530

    Use to define plugin name

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,4 @@
 <?php
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
@@ -20,3 +16,4 @@ function _manually_load_plugin() {
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 require $_tests_dir . '/includes/bootstrap.php';
+
