0ff7a10a5a6108f336dd52efc0d5636efd9060b5
Dipak C. Gajjar <me@dipakgajjar.com>
2019-07-03 17:31:15 +0530

    Reverted to old version

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -1,8 +1,4 @@
 <?php
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
 class PluginTest extends WP_UnitTestCase {
   // Check that that activation doesn't break
   function test_plugin_activated() {
